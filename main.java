cb2e2b1ba22fef6a1f1db95d7f0c477b1108d37f
Munna727 <155368911+Munna727@users.noreply.github.com>
2024-01-02 20:05:50 +0530

    Update main.java
    here i optimized the program now it can specify each character in a given string

--- main.java
+++ main.java
@@ -1,28 +1,31 @@
-class HelloWorld {
+import java.util.Scanner;
+
+class charspec {
     public static void main(String[] args) {
-      char ch='7';
-      if(ch>=65&&ch<=90||ch>=97&&ch<=122)
-      {
-      if(ch>=65&&ch<=90)
-      {
-          System.out.println("it is an upper case");
-          
-      }
-      else
-      {
-          System.out.println("it is an lower case");
-      }
-    }
-    else
-    {
-        if(ch>=49&&ch<=57)
+       
+        
+        String i="h4t@Rfs@^%#g*";
+        for(int x=0;x<=i.length();x++)
         {
-              System.out.println("it is an number");
+            char ch=i.charAt(x);
+            if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+            if (ch >= 65 && ch <= 90) {
+                System.out.println(ch+"->It is an uppercase letter.");
+            } else {
+                System.out.println(ch+
+                "->It is a lowercase letter.");
+            }
+        } else {
+            if (ch >= 48 && ch <= 57) {
+                System.out.println("It is a number.");
+            } else {
+                System.out.println(ch+"->It is a symbol.");
+            }
         }
-        else
-        {
-             System.out.println("it is an symbol");
+        // Use nextInt() to read an integer
+        
+
+        
         }
     }
-}
 }
